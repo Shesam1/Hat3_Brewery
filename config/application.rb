b269dd68fa23c27b8a79de69e8b65db20f415eea
Sherill Samuel <sherill.samuel@gmail.com>
2018-03-20 19:45:07 -0400

    Added config.assets.initialize_on_precompile = false to config folder application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,22 @@ module HAT3
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.assets.initialize_on_precompile = false
+
+    config.assets.precompile << Proc.new do |path|
+      if path =~ /\.(css|js)\z/
+        full_path = Rails.application.assets.resolve(path).to_path
+        app_assets_path = Rails.root.join('app', 'assets').to_path
+        if full_path.starts_with? app_assets_path
+          puts "including asset: " + full_path
+          true
+        else
+          puts "excluding asset: " + full_path
+          false
+        end
+      else
+        false
+      end
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
